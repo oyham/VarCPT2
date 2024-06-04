976a94e4dd285d2a33146ac8b61a22deb9d79388
oyham <voyhamburu@gmail.com>
2024-06-04 16:20:39 -0300

    add phone button

--- js/index.js
+++ js/index.js
@@ -10,3 +10,27 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   nav_menu.classList.remove("active");
 }))
+
+document.addEventListener('DOMContentLoaded', function() {
+  const phoneButton = document.getElementById('phoneButton');
+  const contactInfo = document.getElementById('contactInfo');
+
+  // Mostrar/ocultar el contenedor de información al hacer clic en el botón del teléfono
+  phoneButton.addEventListener('click', function(event) {
+    contactInfo.classList.toggle('show');
+    event.stopPropagation(); // Evita que el clic en el botón cierre inmediatamente el contenedor
+  });
+
+  // Ocultar el contenedor de información al hacer clic en cualquier lugar fuera de él
+  document.addEventListener('click', function(event) {
+    if (contactInfo.classList.contains('show')) {
+      contactInfo.classList.remove('show');
+    }
+  });
+
+  // Evita que el clic dentro del contenedor de información cierre el contenedor
+  contactInfo.addEventListener('click', function(event) {
+    event.stopPropagation();
+  });
+});
+
